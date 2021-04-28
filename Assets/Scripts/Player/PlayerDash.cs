1170ee61cb88640a4b9461db6f09f23ba1aacc9b
luizatvieira <luizatvieira@yahoo.com.br>
2021-04-28 17:38:38 -0300

    Add dash effect

--- Assets/Scripts/Player/PlayerDash.cs
+++ Assets/Scripts/Player/PlayerDash.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerDash : MonoBehaviour 
 {
+    public GameObject dashEffect;
+
     [Header("Physics")]
     private Rigidbody rb;
     [SerializeField] private float dashSpeed = 10f;
@@ -29,8 +31,13 @@ public class PlayerDash : MonoBehaviour
     {
         isDashing = true;
         canDash = false;
+        GameObject thisDash = Instantiate( dashEffect, transform.position, Quaternion.identity );
+
         yield return new WaitForSeconds( dashTime );
+        Destroy( thisDash );
         isDashing = false;
+        rb.velocity = new Vector3(0.0f,0.0f,0.0f);
+
         yield return new WaitForSeconds( waitTime );
         canDash = true;
     }
