2c2de05da01672ac413f7fe50e2c7c5cfa8438b1
luizatvieira <luizatvieira@yahoo.com.br>
2021-04-28 19:08:51 -0300

    Fix player colisions

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,8 +7,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementSpeed = 1;
 
     public void Move ( Vector2 movementVector, Rigidbody rb ) {
-        Vector3 movement = new Vector3( movementVector.x, 0.0f, movementVector.y );
+        Vector3 movement = new Vector3( 
+            movementVector.x * movementSpeed, 
+            rb.velocity.y, 
+            movementVector.y *movementSpeed 
+        );
         //rb.AddForce( movement * movementSpeed );
-        rb.MovePosition( rb.position + (movement * movementSpeed * Time.deltaTime) );
+        rb.velocity = movement;
     }
 }
